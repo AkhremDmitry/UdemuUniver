8525a1f4e2c21caad8cc4c62e7b3ccae856b4e16
Dzmitry Akhrem <dzmitry.akhrem@ihg.com>
2018-12-18 10:36:19 +0300

    Added button click listener to Student Main layout factory

--- univers-ui/src/main/java/com/felix/ui/students/AddStudentMainLayoutFactory.java
+++ univers-ui/src/main/java/com/felix/ui/students/AddStudentMainLayoutFactory.java
@@ -4,14 +4,16 @@ import com.felix.model.entity.Student;
 import com.felix.utils.Gender;
 import com.felix.utils.StudentsStringUtils;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.fieldgroup.PropertyId;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ValoTheme;
 
 @org.springframework.stereotype.Component
 public class AddStudentMainLayoutFactory {
 
-    private class AddStudentMainLayout extends VerticalLayout{
+    private class AddStudentMainLayout extends VerticalLayout implements Button.ClickListener{
 
         @PropertyId("firstName")
         private TextField firstNameTextField; //if we use name different from Student field's name
@@ -40,6 +42,9 @@ public class AddStudentMainLayoutFactory {
 
             saveButton.setStyleName(ValoTheme.BUTTON_FRIENDLY);
             clearButton.setStyleName(ValoTheme.BUTTON_PRIMARY);
+
+            saveButton.addClickListener(this);
+            clearButton.addClickListener(this);
 
             gender.addItem(Gender.MALE.getString());
             gender.addItem(Gender.FEMALE.getString());
@@ -73,7 +78,30 @@ public class AddStudentMainLayoutFactory {
             return gridLayout;
         }
 
+        public void buttonClick(ClickEvent event) {
+            if (event.getSource() == this.saveButton){
+              save();
+            } else {
+              clearField();
+            }
+        }
 
+        private void save(){
+          try {
+            fieldGroup.commit();
+          } catch (CommitException e) {
+            e.printStackTrace();
+          }
+
+          System.out.println(student);
+        }
+
+        private void clearField() {
+            firstNameTextField.setValue(null);
+            lastName.setValue(null);
+            age.setValue(null);
+            gender.setValue(null);
+        }
     }
 
     public Component createComponet() {
