0f9373865f38dc5f2d45430b608f1098cb8d5615
Dzmitry Akhrem <dzmitry.akhrem@ihg.com>
2018-12-18 13:55:40 +0300

    added JPA annotations

--- univers-model/src/main/java/com/felix/model/entity/Student.java
+++ univers-model/src/main/java/com/felix/model/entity/Student.java
@@ -1,25 +1,39 @@
 package com.felix.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "STUDENT")
 public class Student {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Integer id;
 
     @NotNull(message = "You have to specify first name")
+    @Column(name = "first_name")
     private String firstName;
 
     @NotNull(message = "You have to specify last name")
+    @Column(name = "last_name")
     private String lastName;
 
     @NotNull(message = "You have to specify age")
     @Min(message = "Minimum value is 0", value = 0)
     @Max(value = 100, message = "Minimum value is 0")
+    @Column
     private Integer age;
 
     @NotNull(message = "Gender must be set!")
+    @Column
     private String gender;
 
     public Student() {
